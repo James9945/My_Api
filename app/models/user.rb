7cd2be785adb012fb4ebbe8e30cee3477722eac6
james_p <jamesphilipbp@gmail.com>
2025-02-25 03:16:11 -0800

    Worked on Doorkeeper

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+        
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+      user&.valid_password?(password) ? user : nil
+  end
+        
+        
+
+
 end
+
+
